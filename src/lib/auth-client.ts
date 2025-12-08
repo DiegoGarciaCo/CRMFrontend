0d969492f82e29d315a21b3311cd00317812c602
DiegoGarciaCo <diegogarcia51916@gmail.com>
2025-12-07 21:06:10 -0600

    Updated auth client base url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -25,5 +25,5 @@ export const authClient = createAuthClient({
             window.location.href = "/auth/2fa"
         }
     })],
-    baseURL: "http://localhost:3000"
+    baseURL: process.env.BETTER_AUTH_URL,
 })
